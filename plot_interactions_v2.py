14536008682229c1c9dc8e26fc4efa629b036cde
yair <yaia1309@gmail.com>
2016-03-26 16:36:45 +0300

    show classification interactions

---
+++ plot_interactions_v2.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def calc_density(feature_i, feature_j, arr, res):
+    density = np.zeros((res, res))
+    step = 1.0 / res
+    for step_i in range(res):
+        for step_j in range(res):
+            cond_i = np.logical_and(arr[:, feature_i] >= (step_i * step),
+                                    arr[:, feature_i] < ((step_i + 1) * step))
+            cond_j = np.logical_and(arr[:, feature_j] >= (step_j * step),
+                                    arr[:, feature_j] < ((step_j + 1) * step))
+            density[step_i, step_j] = np.sum(np.logical_and(cond_i, cond_j))
+    density /= np.sum(density)
+    return density
+
+train_data = pd.read_csv('numerai_training_data.csv')
+train_target = train_data.target.values
+del train_data['target']
+train_data = np.array(train_data)
+
+# Split into 0, 1 trainsets
+train0 = train_data[train_target == 0, :]
+train1 = train_data[train_target == 1, :]
+
+# For each interaction
+resolution = 30
+n_rows = int(np.sqrt(train_data.shape[1]))
+for i in range(train_data.shape[1]):
+    plt.figure(1)
+    for j in range(i + 1, train_data.shape[1]):
+        plt.subplot(train_data.shape[1] / n_rows, n_rows, j)
+        # Create density map
+        density_map0 = calc_density(i, j, train0, resolution)
+        density_map1 = calc_density(i, j, train1, resolution)
+        plt.imshow((density_map1 / density_map0), origin='lower')
+        plt.title('%d, %d' % (i, j))
+    plt.show()
