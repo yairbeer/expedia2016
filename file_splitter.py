ddc40d48a78ecf844962e329fa632c4657e6d20d
yair <yaia1309@gmail.com>
2016-04-22 17:34:08 +0300

    train/test split works

--- file_splitter.py
+++ file_splitter.py
@@ -2,6 +2,7 @@ import csv
 import pandas as pd
 import numpy as np
 import datetime
+import scipy.stats as stats
 from ml_metrics import mapk
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.cross_validation import train_test_split
@@ -10,36 +11,58 @@ from sklearn.cross_validation import train_test_split
 def parse_dates(df):
     search_date = list(df['date_time'])
     search_date = list(map(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d %H:%M:%S'), search_date))
-    df['search_weekday'] = list(map(lambda x: int(x.strftime('%w')), search_date))
-    df['search_month'] = list(map(lambda x: int(x.strftime('%m')), search_date))
-    df['search_monthday'] = list(map(lambda x: int(x.strftime('%d')), search_date))
+    # df['search_weekday'] = list(map(lambda x: int(x.strftime('%w')), search_date))
+    # df['search_month'] = list(map(lambda x: int(x.strftime('%m')), search_date))
+    # df['search_monthday'] = list(map(lambda x: int(x.strftime('%d')), search_date))
     del df['date_time']
     chkin_date = list(df['srch_ci'])
     chkin_date = list(map(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d'), chkin_date))
-    df['ci_weekday'] = list(map(lambda x: int(x.strftime('%w')), chkin_date))
-    df['ci_month'] = list(map(lambda x: int(x.strftime('%m')), chkin_date))
-    df['ci_monthday'] = list(map(lambda x: int(x.strftime('%d')), chkin_date))
+    # df['ci_weekday'] = list(map(lambda x: int(x.strftime('%w')), chkin_date))
+    # df['ci_month'] = list(map(lambda x: int(x.strftime('%m')), chkin_date))
+    # df['ci_monthday'] = list(map(lambda x: int(x.strftime('%d')), chkin_date))
     del df['srch_ci']
     chkout_date = list(df['srch_co'])
     chkout_date = list(map(lambda x: datetime.datetime.strptime(x, '%Y-%m-%d'), chkout_date))
-    df['co_weekday'] = list(map(lambda x: int(x.strftime('%w')), chkout_date))
-    df['co_month'] = list(map(lambda x: int(x.strftime('%m')), chkout_date))
-    df['co_monthday'] = list(map(lambda x: int(x.strftime('%d')), chkout_date))
+    # df['co_weekday'] = list(map(lambda x: int(x.strftime('%w')), chkout_date))
+    # df['co_month'] = list(map(lambda x: int(x.strftime('%m')), chkout_date))
+    # df['co_monthday'] = list(map(lambda x: int(x.strftime('%d')), chkout_date))
     del df['srch_co']
     return df
 
+
+def percent2mapk(predict_percent, k):
+    predict_map = []
+    for i_row, pred_row in enumerate(predict_percent):
+        predict_map.append([])
+        ranked_row = list(stats.rankdata(pred_row, method='ordinal'))
+        for op_rank in range(k):
+            predict_map[i_row].append(ranked_row.index(n_classes - op_rank - 1))
+    return predict_map
+
+
+def list2str(predict_list, join_by):
+    return predict_list
+
+
+def y2list(y_array):
+    y_list = []
+    for actual in y_array:
+        y_list.append([actual])
+    return y_list
 """
 Variables
 """
+# Number of classes
+n_classes = 100
 # Sampling rate of the data
 samp = 100
-# Number of rows read
+# Number of rows for train
 n_rows = 1e5
 # Whether to merge the data
 merge = False
 # sample_train filename, None if not required
 train_file = 'input/train_samp_%d_merged.csv' % samp
-classifier = RandomForestClassifier(n_estimators=50, max_depth=15, max_features=0.3)
+classifier = RandomForestClassifier(n_estimators=50, max_depth=15)
 
 """
 Read data
@@ -49,16 +72,18 @@ destinations = pd.DataFrame.from_csv('input/destinations.csv')
 # print(destinations)
 
 # Read and sample train
+train_rows = 0
 with open('input/train.csv', newline='') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     columns = spamreader.__next__()
     print('Read the train table columns')
     train_samp = []
     for i, row in enumerate(spamreader):
-        if n_rows < i:
-            break
         if i % samp == 0:
             train_samp.append(row)
+            train_rows += 1
+            if train_rows == n_rows:
+                break
         if i % 1e6 == 0:
             print(i)
 csvfile.close()
@@ -111,9 +136,19 @@ train_samp = parse_dates(train_samp)
 print(train_samp.columns.values)
 
 """
-MLing
+MLing, CV
 """
 X_train, X_test, y_train, y_test = train_test_split(train_samp.values, target.values, test_size=0.33, random_state=42)
 classifier.fit(X_train, y_train)
 train_predict_prob = classifier.predict_proba(X_test)
 print(np.sum(y_test == classifier.predict(X_test)) / y_test.shape[0])
+train_predict_map = percent2mapk(train_predict_prob, 5)
+y_test_list = y2list(y_test)
+print(mapk(y_test_list, train_predict_map, k=5))
+
+"""
+MLing
+"""
+# classifier.fit(train_samp.values, target.values)
+# test_predict_prob = classifier.predict_proba(test.values)
+# test_predict_map = percent2map(test_predict_prob, 5)
